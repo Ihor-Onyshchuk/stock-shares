b2383fa6ecf490ba7d2d59e682ff92eb8c0e5a32
ihor <onyshchukihor11@gmail.com>
2020-10-03 14:49:17 +0300

    add getStockInfo function

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import { API_TOKEN } from '../api-token';
 
 const http = axios.create({
   baseURL: 'https://cloud.iexapis.com/stable',
 });
+
+export const getStockInfo = () => (
+  http.get(`/stock/market/list/mostactive?&listLimit=10&token=${API_TOKEN}`)
+);
