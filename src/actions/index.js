bcdab1c3095a48a3936ba99b31993310079f9c6c
ihor <onyshchukihor11@gmail.com>
2020-10-03 16:28:44 +0300

    add actions

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+import {getStockInfo} from '../api';
+
+export const FETCH_DATA_REQUEST = 'FETCH_DATA_REQUEST';
+export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+
+export const fetchDataRequest = () => ({
+  type: FETCH_DATA_REQUEST
+});
+
+export const fetchDataFailure = () => ({
+  type: FETCH_DATA_FAILURE,
+});
+
+export const fetchDataSuccess = (result) => ({
+  type: FETCH_DATA_SUCCESS,
+  result
+})
+
+export const fetchData = () => (dispatch) => {
+  dispatch(fetchDataRequest());
+  getStockInfo()
+    .then((response) => dispatch(fetchDataSuccess(response.data || [])))
+    .catch(() => dispatch(fetchUsersFailure()))
+}
